aa9bca8f97db7a4ba2b0776da6f382c2544d5520
nigel5 <nigel5@192.168.0.30>
2019-10-09 05:20:34 +0100

    Added temp reading for the DS18b20 temp sensor

--- sh/gstation_tick.php
+++ sh/gstation_tick.php
@@ -10,28 +10,54 @@ echo "Location: " . $loc . " (" . latToDms ( $lat ) . ", " . lngToDms ( $lng ) .
 
 tick ();
 
-$cmd = "python3 " . dirname ( __FILE__ ) . "/dht22.py 2>&1";
-ob_start ();
-$last_line = @system ( $cmd, $retval );
-// $last_line = "T:25.9|H:53.3";
-// $c = ob_get_contents ();
-ob_end_clean ();
+$use_dht = false;
+if ($use_dht) {
+	$cmd = "python3 " . dirname ( __FILE__ ) . "/dht22.py 2>&1";
+	ob_start ();
+	$last_line = @system ( $cmd, $retval );
+	// $last_line = "T:25.9|H:53.3";
+	// $c = ob_get_contents ();
+	ob_end_clean ();
 
-if ($last_line [0] != 'T') {
-	echo "Unable to determine local temp/humidity\n\n";
+	if ($last_line [0] != 'T') {
+		echo "Unable to determine local temp/humidity\n\n";
+	} else {
+		@list ( $temperature, $humidity ) = @explode ( "|", $last_line );
+		$temperature = @explode ( ":", $temperature ) [1] + 0;
+		$humidity = @explode ( ":", $humidity ) [1] + 0;
+
+		$ret = $mysql->query ( "REPLACE INTO th_logger (temperature, humidity) VALUES (?, ?)", "dd", array (
+				$temperature,
+				$humidity
+		) );
+
+		echo "Local temperature: " . $temperature . "C\n";
+		echo "Local humidity: " . $humidity . "%\n";
+		echo "\n";
+	}
 } else {
-	@list ( $temperature, $humidity ) = @explode ( "|", $last_line );
-	$temperature = @explode ( ":", $temperature ) [1] + 0;
-	$humidity = @explode ( ":", $humidity ) [1] + 0;
+	$cmd = "cat /sys/bus/w1/devices/28-*/w1_slave 2>&1";
+	ob_start ();
+	$last_line = @system ( $cmd, $retval );
+	//$last_line = "67 01 4c 46 7f ff 0c 10 c4 t=22437";
+	// $last_line = "T:25.9|H:53.3";
+	// $c = ob_get_contents ();
+	ob_end_clean ();
 
-	$ret = $mysql->query ( "REPLACE INTO th_logger (temperature, humidity) VALUES (?, ?)", "dd", array (
-			$temperature,
-			$humidity
-	) );
-	
-	echo "Local temperature: " . $temperature . "C\n";
-	echo "Local humidity: " . $humidity . "%\n";
-	echo "\n";
+	@list ( $dummy, $temperature ) = explode ( " t=", $last_line );
+	if ($temperature == "") {
+		echo "Unable to determine local temp\n\n";
+	} else {
+		$temperature = ($temperature + 0) / 1000;
+
+		$ret = $mysql->query ( "REPLACE INTO th_logger (temperature, humidity) VALUES (?, ?)", "dd", array (
+				$temperature,
+				999
+		) );
+
+		echo "Local temperature: " . $temperature . "C\n";
+		echo "\n";
+	}
 }
 
 logger ( LL_DEBUG, "system command: '" . $cmd . "'" );
