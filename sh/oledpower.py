e79f0a9aa3366108b97b2655b23b2d9170bfce3a
Nigel Johnson <nigel@nigeljohnson.net>
2019-10-13 07:27:42 +0100

    Added second button on GPIO14 for PCB

--- sh/oledpower.py
+++ sh/oledpower.py
@@ -15,42 +15,48 @@ from datetime import datetime
 import time
 
 # digital button for pressing and the hold time.
-offGPIO = 21
+offGPIO1 = 21
+offGPIO2 = 14
 holdTime = 1
 
-# Text to display
+# Use the big display
+WIDTH = 128
+HEIGHT = 64
+BORDER = 1
+
+# Loop ender... When done, halt the system!!
 done = False
 
 def drawText(text):
-    # Create blank image for drawing.
-    # Make sure to create image with mode '1' for 1-bit color.
-    image = Image.new('1', (oled.width, oled.height))
+	# Create blank image for drawing.
+	# Make sure to create image with mode '1' for 1-bit color.
+	image = Image.new('1', (oled.width, oled.height))
     
-    # Get drawing object to draw on image.
-    draw = ImageDraw.Draw(image)
-    
-    # Draw a white background
-    draw.rectangle((0, 0, oled.width, oled.height), outline=255, fill=255)
-    
-    # Draw a smaller inner rectangle
-    draw.rectangle((BORDER, BORDER, oled.width - BORDER - 1, oled.height - BORDER - 1), outline=0, fill=0)
-    
-     
-    # Draw Some Text
-    font = ImageFont.truetype(os.path.dirname(os.path.abspath(__file__))+"/../fonts/andalemo.ttf", 14)
-    (font_width, font_height) = font.getsize(text)
-    #draw.text((oled.width // 2 - font_width // 2, oled.height // 2 - font_height // 2), text, font=font, fill=255)
-    draw.text((oled.width // 2 - font_width // 2, oled.height // 2 - font_height-1 ), text, font=font, fill=255)
+	# Get drawing object to draw on image.
+	draw = ImageDraw.Draw(image)
 
-    now = datetime.now()
-    ts = now.strftime('%Y-%m-%d %H:%M:%S')
-    font = ImageFont.truetype(os.path.dirname(os.path.abspath(__file__))+"/../fonts/andalemo.ttf", 10)
-    (font_width, font_height) = font.getsize(ts)
-    draw.text((oled.width // 2 - font_width // 2, oled.height // 2 + font_height+1 ), ts, font=font, fill=255)
+	if BORDER > 0 :
+		# Draw a white background
+		draw.rectangle((0, 0, oled.width, oled.height), outline=255, fill=255)
+
+		# Draw a smaller inner rectangle
+		draw.rectangle((BORDER, BORDER, oled.width - BORDER - 1, oled.height - BORDER - 1), outline=0, fill=0)
+
+	# Draw Some Text
+	font = ImageFont.truetype(os.path.dirname(os.path.abspath(__file__))+"/../fonts/andalemo.ttf", 14)
+	(font_width, font_height) = font.getsize(text)
+	#draw.text((oled.width // 2 - font_width // 2, oled.height // 2 - font_height // 2), text, font=font, fill=255)
+	draw.text((oled.width // 2 - font_width // 2, oled.height // 2 - font_height-3 ), text, font=font, fill=255)
+
+	now = datetime.now()
+	ts = now.strftime('%Y-%m-%d %H:%M')
+	font = ImageFont.truetype(os.path.dirname(os.path.abspath(__file__))+"/../fonts/andalemo.ttf", 10)
+	(font_width, font_height) = font.getsize(ts)
+	draw.text((oled.width // 2 - font_width // 2, oled.height // 2 + font_height+3 ), ts, font=font, fill=255)
     
-    # Display image
-    oled.image(image)
-    oled.show()
+	# Display image
+	oled.image(image)
+	oled.show()
 
 # the function called to shut down the RPI - just exist for now
 def shutdown():
@@ -60,14 +66,12 @@ def shutdown():
 
 
 # setup the callback
-btn = Button(offGPIO, hold_time=holdTime)
-btn.when_held = shutdown
+btn1 = Button(offGPIO1, hold_time=holdTime)
+btn1.when_held = shutdown
+btn2 = Button(offGPIO2, hold_time=holdTime)
+btn2.when_held = shutdown
 # pause()  # handle the button presses in the background
 
-# Use the big display
-WIDTH = 128
-HEIGHT = 64
-BORDER = 1
 
 # Use for I2C.
 i2c = board.I2C()
@@ -77,8 +81,8 @@ oled = adafruit_ssd1306.SSD1306_I2C(WIDTH, HEIGHT, i2c, addr=0x3c)
 oled.fill(0)
 oled.show()
 
-drawText("Starting up...")
-print ("Starting up...")
+print("OLED display starting up")
+drawText ("Starting up...")
 
 triggerfile = "/tmp/oled.txt"
 while not done:
@@ -88,7 +92,7 @@ while not done:
 			text = line.strip()
 
 		os.remove(triggerfile)
-		print("Writing '",text,"'")
+		#print("Writing '",text,"'")
 		drawText(text)
 
 	except FileNotFoundError:
@@ -97,6 +101,6 @@ while not done:
 
 	time.sleep(0.5)
 
-print("SHUTDOWN!")
+print("SHUTDOWN")
 #sys.exit()
 os.system("sudo poweroff")
