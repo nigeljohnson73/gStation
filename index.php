a96b9c1d48c83239a5857303463e4069f7b56e1f
nigel5 <nigel5@192.168.0.30>
2019-10-16 00:35:06 +0100

    Simple layout for wider screens

--- index.php
+++ index.php
@@ -35,34 +35,49 @@ $mon = timestampFormat ( timestampNow (), "m" ); // 10;
 <html ng-app>
 <head>
 <title><?php echo $app_title." - ".$loc ?></title>
-<link rel="icon" href="gfx/rhino.png">
-<script
-	src="https://ajax.googleapis.com/ajax/libs/angularjs/1.7.8/angular.min.js"></script>
-<link rel="stylesheet"
-	href="https://netdna.bootstrapcdn.com/twitter-bootstrap/2.0.4/css/bootstrap-combined.min.css">
-<link rel="stylesheet" href="css/app.<?php stylesheetPayload() ?>.css">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  
+  <link rel="icon" href="gfx/rhino.png">
+  
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/app.<?php stylesheetPayload() ?>.css">
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.7.8/angular.min.js"></script>
 <script src='js/app.<?php javascriptPayload() ?>.js'></script>
 </head>
 <body>
 	<div class="container-fluid">
-		<pre><?php
-		// tick();
-		// $status = getConfig("STATUS", "NIGHT");
-		echo "Processing weather at " . timestampFormat ( timestampNow (), "Y-m-d\TH:i:s T" ) . "\n";
-		echo "Location: " . $loc . " (" . latToDms ( $lat ) . ", " . lngToDms ( $lng ) . ")\n";
-		echo "Current status: '" . getConfig ( "STATUS", "---" ) . "'\n";
-		?>
-		
+	&nbsp;
+		<div class="row">
+			<div class="col-sm-4">
+				<pre><?php
+					// tick();
+					// $status = getConfig("STATUS", "NIGHT");
+					echo "Processing weather at " . timestampFormat ( timestampNow (), "Y-m-d\TH:i:s T" ) . "\n";
+					echo "Location: " . $loc . " (" . latToDms ( $lat ) . ", " . lngToDms ( $lng ) . ")\n";
+					echo "Current status: '" . getConfig ( "STATUS", "---" ) . "'\n";
+?>
 Current data: <?php print_r(getModel(timestampNow())) ?>
 
 Model status: <?php print_r(modelStatus()) ?>
 
-Last temp: <?php print_r(tfn(lastTemp())) ?>
-		</pre>
-	</div>
+Last temp: <?php print_r(tfn(lastTemp())) ?></pre>
+		</div>
+	
+		<div class="col-sm-8 text-center">
+				<img src="gfx/graph_local_temperatures.php?<?php echo randomQuery() ?>" alt="Local actual temperature graph" class="img-thumbnail" style="margin-bottom:8px; margin-right:5px;" /> 
+				<img src="gfx/graph_schedule_temperatures.php" alt="Remote average temperature graph" class="img-thumbnail" style="margin-bottom:8px; margin-right:5px;" />
+				<img src="gfx/graph_schedule_sun.php" alt="Remote sunrise and sunset graph" class="img-thumbnail" style="margin-bottom:8px; margin-right:5px;" />
+				<img src="gfx/graph_schedule_daylight.php" alt="Remote day length graph" class="img-thumbnail" style="margin-bottom:8px; margin-right:5px;" />
+		</div>
+	</div> <!-- ROW --> 
 
+	&nbsp;
+	<div class="row">
 	<div class="container-fluid debug">
-
 		<pre><?php
 		$str = $logger->getString ();
 		if (strlen ( trim ( $str ) ) == 0) {
@@ -73,12 +88,7 @@ Last temp: <?php print_r(tfn(lastTemp())) ?>
 		echo "Log output:\n";
 		echo $str . "\n";
 		?></pre>
-
-		<img src="gfx/graph_local_temperatures.php?<?php echo randomQuery() ?>" alt="Local actual temperature graph" /> 
-		<img src="gfx/graph_schedule_temperatures.php" alt="Remote average temperature graph" />
-		<img src="gfx/graph_schedule_sun.php" alt="Remote sunrise and sunset graph" />
-		<img src="gfx/graph_schedule_daylight.php" alt="Remote day length graph" />
-		<!-- <?php //phpInfo() ?> -->
+	</div>
 	</div>
 </body>
 </html>
