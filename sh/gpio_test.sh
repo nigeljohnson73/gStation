a84cc0f8730070e169ee00dee787b44a76ab8047
Nigel Johnson <nigel@nigeljohnson.net>
2019-10-26 21:16:38 +0100

    Updated output logging

--- sh/gpio_test.sh
+++ sh/gpio_test.sh
@@ -12,8 +12,10 @@ then
 fi
 
 while [ 1 ]; do 
+	echo "Switching pin $1 ON"
 	sh /webroot/gStation/sh/gpio.sh $1 0
 	sleep 1
+	echo "Switching pin $1 OFF"
 	sh /webroot/gStation/sh/gpio.sh $1 1
 	sleep 1
 done
