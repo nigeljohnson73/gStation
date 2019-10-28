07853a7380ebd4cd0c37be5e7f4059c711c15c6c
nigel5 <nigel5@192.168.0.30>
2019-10-28 16:55:51 +0000

    Formatted file

--- include/weather.php
+++ include/weather.php
@@ -40,7 +40,7 @@ function clearSensorLogger() {
 	global $mysql, $logger;
 	setupTables ();
 	$mysql->query ( "DELETE FROM temperature_logger where entered < DATE_SUB(NOW(), INTERVAL 24 HOUR)" );
-	$logger->clearLogs();
+	$logger->clearLogs ();
 }
 
 function lastTemp() {
@@ -764,21 +764,21 @@ function nextSunChange() {
 	$ret = "";
 	if ($nowoffset < $model [timestampFormat ( $today, "md" )]->sunriseOffset) {
 		$secs = $model [timestampFormat ( $today, "md" )]->sunriseOffset - $nowoffset;
-		if($secs>60) {
+		if ($secs > 60) {
 			$ret = "Sunrise " . periodFormat ( $secs, true );
 		} else {
 			$ret = "Sunrise < 1m";
 		}
 	} elseif ($nowoffset < $model [timestampFormat ( $today, "md" )]->sunsetOffset) {
 		$secs = $model [timestampFormat ( $today, "md" )]->sunsetOffset - $nowoffset;
-		if($secs >60) {
+		if ($secs > 60) {
 			$ret = "Sunset " . periodFormat ( $secs, true );
 		} else {
 			$ret = "Sunset < 1m";
 		}
 	} else {
 		$secs = $model [timestampFormat ( $tomorrow, "md" )]->sunriseOffset - $nowoffset + (24 * 60 * 60);
-		if($secs >60) {
+		if ($secs > 60) {
 			$ret = "Sunrise " . periodFormat ( $secs, true );
 		} else {
 			$ret = "Sunrise < 1m";
