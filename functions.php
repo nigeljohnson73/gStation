f691ef671e096ea08baed8e5e57a9517c514598b
nigel5 <nigel5@192.168.1.140>
2020-01-07 22:17:59 +0000

    removed logging of bit compare

--- functions.php
+++ functions.php
@@ -21,7 +21,7 @@ function getAppVersion() {
 function newestFile($p = ".") {
 	$mtime = 0;
 	$mfile = "";
-	
+
 	$files = directoryListing ( $p );
 	foreach ( $files as $file ) {
 		$fn = str_replace ( $p . "/", "", $file );
@@ -41,7 +41,7 @@ function newestFile($p = ".") {
 			}
 		}
 	}
-	
+
 	// echo "newest file in '".$p."': '".$mfile."' - ".date("Y/m/d H:i:s", $mtime)."\n";
 	return array (
 			$mtime,
@@ -63,25 +63,25 @@ function bitCompare($str, $in, $now, $prev) {
 	$now_str = "NOW";
 	$no_str = "NEVER";
 	$ret = $no_str;
-	
+
 	$hi = str_pad ( decbin ( $in ), $bp, "0", STR_PAD_LEFT );
 	$hn = str_pad ( decbin ( $now ), $bp, "0", STR_PAD_LEFT );
 	$hp = str_pad ( decbin ( $prev ), $bp, "0", STR_PAD_LEFT );
-	
-	echo $str . ": ";
+
+	// echo $str . ": ";
 	if ($in & $now) {
 		$ret = $now_str;
 	} elseif ($in & $prev) {
 		$ret = $has_str;
 	}
-	echo $ret;
-	echo "\n";
-	
-	echo "In  (in): " . $hi . " (" . dechex ( $in ) . ")\n";
-	echo "Now (hn): " . $hn . " (" . (($in & $now) ? ("set") : ("--")) . ")\n";
-	echo "Prev(hp): " . $hp . " (" . (($in & $prev) ? ("set") : ("--")) . ")\n";
-	echo "----\n";
-	
+	// echo $ret;
+	// echo "\n";
+
+	// echo "In (in): " . $hi . " (" . dechex ( $in ) . ")\n";
+	// echo "Now (hn): " . $hn . " (" . (($in & $now) ? ("set") : ("--")) . ")\n";
+	// echo "Prev(hp): " . $hp . " (" . (($in & $prev) ? ("set") : ("--")) . ")\n";
+	// echo "----\n";
+
 	return $ret;
 }
 
@@ -89,7 +89,7 @@ function GUID() {
 	if (function_exists ( 'com_create_guid' ) === true) {
 		return trim ( com_create_guid (), '{}' );
 	}
-	
+
 	return sprintf ( '%04X%04X-%04X-%04X-%04X-%04X%04X%04X', mt_rand ( 0, 65535 ), mt_rand ( 0, 65535 ), mt_rand ( 0, 65535 ), mt_rand ( 16384, 20479 ), mt_rand ( 32768, 49151 ), mt_rand ( 0, 65535 ), mt_rand ( 0, 65535 ), mt_rand ( 0, 65535 ) );
 }
 
@@ -124,7 +124,7 @@ function decimateArray($arr, $n = 2) {
 	$i = 0;
 	$k = null;
 	$v = array ();
-	
+
 	$ret = array ();
 	foreach ( $arr as $ak => $av ) {
 		if ($i == $n) {
@@ -136,12 +136,12 @@ function decimateArray($arr, $n = 2) {
 			$v = array ();
 		}
 		$i += 1;
-		
+
 		if ($k === null) {
 			// echo "Stash key: i: $i, ak: $ak\n";
 			$k = $ak;
 		}
-		
+
 		// echo "Adding value '$av'\n";
 		$v [] = $av;
 	}
@@ -183,7 +183,7 @@ function _smoothArray($arr, $n = 1) {
 function averageObjectArray($arr) {
 	// A place for all the field data and the values
 	$store = array ();
-	
+
 	// Iterate through each object in the array
 	foreach ( $arr as $i ) {
 		$keys = array_keys ( ( array ) $i );
@@ -200,7 +200,7 @@ function averageObjectArray($arr) {
 			}
 		}
 	}
-	
+
 	// start as an array so I can sort the keys later
 	$ret = array ();
 	// Now iterate through each field that was collected
@@ -209,10 +209,10 @@ function averageObjectArray($arr) {
 		$v = array_sum ( $vals ) / count ( $vals );
 		$ret [$k] = $v;
 	}
-	
+
 	// Now sort the array based on key names
 	ksort ( $ret );
-	
+
 	// return a new object from the array
 	return ( object ) $ret;
 }
@@ -249,7 +249,7 @@ function latToDms($decimal) {
 	$m = 0;
 	$s = 0;
 	$dir = "X";
-	
+
 	decimalToDmsRaw ( $decimal, $d, $m, $s, $dir, true );
 	return dmsFormat ( $d, $m, $s, $dir );
 }
@@ -259,7 +259,7 @@ function lngToDms($decimal) {
 	$m = 0;
 	$s = 0;
 	$dir = "X";
-	
+
 	decimalToDmsRaw ( $decimal, $d, $m, $s, $dir, false );
 	return dmsFormat ( $d, $m, $s, $dir );
 }
@@ -279,10 +279,10 @@ function decimalToDmsRaw($decimal, &$degrees, &$minutes, &$seconds, &$direction,
 	if (! is_numeric ( $decimal ) || abs ( $decimal ) > 180 || ! is_bool ( $latitude )) {
 		return false;
 	}
-	
+
 	// inputs OK, proceed
 	// type is latitude when true, longitude when false
-	
+
 	// set direction; north assumed
 	if ($latitude && $decimal < 0) {
 		$direction = 'S';
@@ -293,19 +293,19 @@ function decimalToDmsRaw($decimal, &$degrees, &$minutes, &$seconds, &$direction,
 	} else {
 		$direction = 'N';
 	}
-	
+
 	// get absolute value of decimal
 	$d = abs ( $decimal );
-	
+
 	// get degrees
 	$degrees = floor ( $d );
-	
+
 	// get seconds
 	$seconds = ($d - $degrees) * 3600;
-	
+
 	// get minutes
 	$minutes = floor ( $seconds / 60 );
-	
+
 	// reset seconds
 	$seconds = floor ( $seconds - ($minutes * 60) );
 }
@@ -344,9 +344,9 @@ function timestamp2Time($ts) {
 	$ts = str_replace ( ".", "", $ts );
 	$ts = preg_replace ( "/[A-Z]*/", "", strtoupper ( $ts ) );
 	$ts .= "000000"; // just in case I only suply a date
-	
+
 	// echo "timestamp2Time($ts): New ts: '$ts'\n";
-	
+
 	$year = substr ( $ts, 0, 4 );
 	$month = substr ( $ts, 4, 2 );
 	$day = substr ( $ts, 6, 2 );
@@ -380,7 +380,7 @@ function periodFormat($secs, $short = false) {
 			$estr .= $pl;
 		}
 	}
-	
+
 	$mins = $h - $hours;
 	$mins *= 60;
 	if ($mins) {
@@ -407,16 +407,16 @@ function durationFormat($secs, $use_nearest_sec = false) {
 	$sec_min = 60;
 	$sec_hour = $sec_min * 60;
 	$sec_day = $sec_hour * 24;
-	
+
 	$days = floor ( $secs / $sec_day );
 	$secs -= $days * $sec_day;
-	
+
 	$hours = floor ( $secs / $sec_hour );
 	$secs -= $hours * $sec_hour;
-	
+
 	$mins = floor ( $secs / $sec_min );
 	$secs -= $mins * $sec_min;
-	
+
 	$ret = "";
 	if ($days > 0) {
 		$ret .= " " . $days . "d";
@@ -430,28 +430,28 @@ function durationFormat($secs, $use_nearest_sec = false) {
 	if ($use_nearest_sec) {
 		$ret .= " " . $secs . "s";
 	} else {
-		$ret .= " " . number_format($secs, 3) . "s";
+		$ret .= " " . number_format ( $secs, 3 ) . "s";
 	}
-	
+
 	return trim ( $ret );
 }
 
 function durationStamp($secs, $use_sec = false) {
 	$secs = nearest ( $secs, 1 );
-	
+
 	$sec_min = 60;
 	$sec_hour = $sec_min * 60;
 	$sec_day = $sec_hour * 24;
-	
+
 	$days = floor ( $secs / $sec_day );
 	$secs -= $days * $sec_day;
-	
+
 	$hours = floor ( $secs / $sec_hour );
 	$secs -= $hours * $sec_hour;
-	
+
 	$mins = floor ( $secs / $sec_min );
 	$secs -= $mins * $sec_min;
-	
+
 	$st = true; // set to false for growing min hr etc
 	$ret = "";
 	if ($st || $days > 0) {
@@ -469,7 +469,7 @@ function durationStamp($secs, $use_sec = false) {
 	if ($use_sec && ($st || $secs > 0)) {
 		$ret .= " " . sprintf ( "%02d", $secs ) . "s";
 	}
-	
+
 	return trim ( $ret );
 }
 
@@ -499,13 +499,13 @@ function timestampDifference($tthen, $tnow) {
 
 function timestampDelta($lower, $upper) {
 	$seconds_diff = timestampDifference ( $lower, $upper );
-	
+
 	$pre = "";
 	$day = 24 * 3600;
 	$week = 7 * $day;
 	$twenty_weeks = 20 * $week;
 	$year = 365 * $day;
-	
+
 	if ($seconds_diff < $week) {
 		// $days = round($seconds_diff / $day);
 		$days = floor ( $seconds_diff / $day );
@@ -575,19 +575,19 @@ function obfuscateString($email) {
 function obfuscateAllEmailAddresses($text) {
 	$text = ' ' . $text . ' ';
 	$text = preg_replace ( "#(([A-Za-z0-9\-_\.]+?)@([^\s,{}\(\)\[\]]+\.[^\s.,{}\(\)\[\]]+))#iesU", "obfuscateString(\"$1\")", $text );
-	
+
 	return substr ( $text, 1, strlen ( $text ) - 2 );
 }
 
 function nearest($number, $nearest, $updown = null) {
 	$bits = floor ( $number / $nearest );
 	$result = $bits * $nearest;
-	
+
 	if ($result != $number) {
 		switch ($updown) {
 			case 'down' :
 				break;
-				
+
 			case 'up' :
 				$result += $nearest;
 				break;
@@ -628,13 +628,13 @@ function latLongRadiusMinMax($latitude, $longitude, $km) {
 	$lng_max = $longitude + $radius / abs ( cos ( deg2rad ( $latitude ) ) * 69 );
 	$lat_min = $latitude - ($radius / 69);
 	$lat_max = $latitude + ($radius / 69);
-	
+
 	$arr = array ();
 	$arr ["lat_min"] = $lat_min;
 	$arr ["lat_max"] = $lat_max;
 	$arr ["long_min"] = $lng_min;
 	$arr ["long_max"] = $lng_max;
-	
+
 	return $arr;
 }
 
@@ -649,17 +649,17 @@ function latLongDistance($lat1, $lon1, $lat2, $lon2) {
 
 function getLocalJavascriptPayload() {
 	$str = "";
-	
+
 	if (strtolower ( @ $_SERVER ['SERVER_NAME'] ) == "localhost") {
 		$str = "\tlogging_enabled = true;\n" . $str;
 	} else {
 	}
-	
+
 	if (isset ( $_GET ["packed"] ) || strtolower ( @ $_SERVER ['SERVER_NAME'] ) != "localhost") {
 		$packer = new JavaScriptPacker ( $str, 95, false, false );
 		$str = utf8_encode ( $packer->pack () );
 	}
-	
+
 	return $str;
 }
 
@@ -703,7 +703,7 @@ function directoryListing($dirname, $extensoes = null) {
 	} else if (! is_array ( $extensoes )) {
 		$extensoes = explode ( ",", $extensoes );
 	}
-	
+
 	$files = array ();
 	$dir = @ opendir ( $dirname );
 	while ( $dir && false !== ($file = readdir ( $dir )) ) {
@@ -720,7 +720,7 @@ function directoryListing($dirname, $extensoes = null) {
 			}
 		}
 	}
-	
+
 	@ closedir ( $dirname );
 	sort ( $files );
 	return $files;
@@ -743,37 +743,37 @@ function compressJavascript($force = false, $debug = true) {
 	if (@ strtolower ( $_SERVER ['SERVER_NAME'] ) == "localhost") {
 		// Only perform this action if we are on the development server
 		// Production payload files will be updloaded from the dev sever
-		
+
 		// Set up the target filenames for the raw and packed versions
 		$targetfn = dirname ( __FILE__ ) . "/js/app.nopack.js";
 		$ptargetfn = dirname ( __FILE__ ) . "/js/app.packed.js";
 		$ctargetfn = dirname ( __FILE__ ) . "/js/app.nocr.js";
-		
+
 		// Get the current mod time in case any sub files are newer
 		$tt = @ filemtime ( $targetfn );
 		// $tt = 0;
-		
+
 		// An indicator to determine whenther the file needs updating
 		$save = $force;
-		
+
 		if ($debug) {
 			// echo "<!-- compression check: $targetfn -->\n";
 			// echo "<!-- mtime: $tt -->\n";
 		}
-		
+
 		// Get a list of javascript files for the bundle
-		$files = array();
-		$files = array_merge($files, directoryListing ( dirname ( __FILE__ ) . "/js/raw", ".js" ));
-		$files = array_merge($files, directoryListing ( dirname ( __FILE__ ) . "/js/raw/_directives", ".js" ));
-		$files = array_merge($files, directoryListing ( dirname ( __FILE__ ) . "/js/raw/_services", ".js" ));
-		$files = array_merge($files, directoryListing ( dirname ( __FILE__ ) . "/js/raw/_controllers", ".js" ));
-		$files = array_merge($files, directoryListing ( dirname ( __FILE__ ) . "/js/raw/cam", ".js" ));
-		
+		$files = array ();
+		$files = array_merge ( $files, directoryListing ( dirname ( __FILE__ ) . "/js/raw", ".js" ) );
+		$files = array_merge ( $files, directoryListing ( dirname ( __FILE__ ) . "/js/raw/_directives", ".js" ) );
+		$files = array_merge ( $files, directoryListing ( dirname ( __FILE__ ) . "/js/raw/_services", ".js" ) );
+		$files = array_merge ( $files, directoryListing ( dirname ( __FILE__ ) . "/js/raw/_controllers", ".js" ) );
+		$files = array_merge ( $files, directoryListing ( dirname ( __FILE__ ) . "/js/raw/cam", ".js" ) );
+
 		$js = "";
 		foreach ( $files as $file ) {
 			// Add the sub file to the main file
 			$js .= "\n" . trim ( file_get_contents ( $file ) );
-			
+
 			// Check if the sub-file is newer and make for saving
 			if (filemtime ( $file ) > $tt) {
 				if ($debug) {
@@ -786,30 +786,30 @@ function compressJavascript($force = false, $debug = true) {
 				}
 			}
 		}
-		
+
 		// If we should save the file, and there is something to save
 		if ($save && strlen ( $js )) {
 			if ($debug)
 				echo "<!-- Saving $targetfn (" . strlen ( $js ) . " bytes) -->\n";
-				
-				// Write the uncompressed version for testing etc
-				file_put_contents ( $targetfn, $js );
-				
-				// Pack and write the compressed version
-				$packer = new JavaScriptPacker ( $js, 95, false, false );
-				$pjs = utf8_encode ( $packer->pack () );
-				if ($debug)
-					echo "<!-- Saving $ptargetfn (" . strlen ( $pjs ) . " bytes) -->\n";
-					file_put_contents ( $ptargetfn, $pjs );
-					
-					$js = preg_replace ( '!/\*.*?\*/!s', '', $js ); // Remove block comments
-					$js = preg_replace ( '#[^:\'"]//[^\,].*#', '', $js ); // Remove non url single line comments - the comma also allows for javascript regex
-					$js = preg_replace ( '#^//.*#', '', $js ); // remove single line comments that start a line
-					$js = preg_replace ( '/\n/', "", $js ); // remove carriage returns
-					$js = preg_replace ( '/\s+/', " ", $js ); // remove any weird white space
-					if ($debug)
-						echo "<!-- Saving $ctargetfn (" . strlen ( $js ) . " bytes) -->\n";
-						file_put_contents ( $ctargetfn, $js );
+
+			// Write the uncompressed version for testing etc
+			file_put_contents ( $targetfn, $js );
+
+			// Pack and write the compressed version
+			$packer = new JavaScriptPacker ( $js, 95, false, false );
+			$pjs = utf8_encode ( $packer->pack () );
+			if ($debug)
+				echo "<!-- Saving $ptargetfn (" . strlen ( $pjs ) . " bytes) -->\n";
+			file_put_contents ( $ptargetfn, $pjs );
+
+			$js = preg_replace ( '!/\*.*?\*/!s', '', $js ); // Remove block comments
+			$js = preg_replace ( '#[^:\'"]//[^\,].*#', '', $js ); // Remove non url single line comments - the comma also allows for javascript regex
+			$js = preg_replace ( '#^//.*#', '', $js ); // remove single line comments that start a line
+			$js = preg_replace ( '/\n/', "", $js ); // remove carriage returns
+			$js = preg_replace ( '/\s+/', " ", $js ); // remove any weird white space
+			if ($debug)
+				echo "<!-- Saving $ctargetfn (" . strlen ( $js ) . " bytes) -->\n";
+			file_put_contents ( $ctargetfn, $js );
 		}
 	}
 }
@@ -819,32 +819,32 @@ function compressStylesheet($force = false, $debug = true) {
 	if (@ strtolower ( $_SERVER ['SERVER_NAME'] ) == "localhost") {
 		// Only perform this action if we are on the development server
 		// Production payload files will be updloaded from the dev sever
-		
+
 		// Set up the target filenames for the raw and packed versions
 		$targetfn = dirname ( __FILE__ ) . "/css/app.nopack.css";
 		$ptargetfn = dirname ( __FILE__ ) . "/css/app.packed.css";
 		$ctargetfn = dirname ( __FILE__ ) . "/css/app.nocr.css";
-		
+
 		// Get the current mod time in case any sub files are newer
 		$tt = @ filemtime ( $targetfn );
 		// $tt = 0;
-		
+
 		// An indicator to determine whenther the file needs updating
 		$save = $force;
-		
+
 		if ($debug) {
 			// echo "<!-- compression check: $targetfn -->\n";
 			// echo "<!-- mtime: $tt -->\n";
 		}
-		
+
 		// Get a list of stylesheet files for the bundle
 		$files = directoryListing ( dirname ( __FILE__ ) . "/css/raw", ".css" );
-		
+
 		$css = "";
 		foreach ( $files as $file ) {
 			// Add the sub file to the main file
 			$css .= "\n" . trim ( file_get_contents ( $file ) );
-			
+
 			// Check if the sub-file is newer and make for saving
 			if (filemtime ( $file ) > $tt) {
 				if ($debug) {
@@ -857,29 +857,29 @@ function compressStylesheet($force = false, $debug = true) {
 				}
 			}
 		}
-		
+
 		// If we should save the file, and there is something to save
 		if ($save && strlen ( $css )) {
 			if ($debug)
 				echo "<!-- Saving $targetfn (" . strlen ( $css ) . " bytes) -->\n";
-				
-				// Write the uncompressed version for testing etc
-				file_put_contents ( $targetfn, $css );
-				
-				// Pack and write the compressed version
-				$pcss = CssMin::minify ( $css );
-				if ($debug)
-					echo "<!-- Saving $ptargetfn (" . strlen ( $pcss ) . " bytes) -->\n";
-					file_put_contents ( $ptargetfn, $pcss );
-					
-					$css = preg_replace ( '!/\*.*?\*/!s', '', $css ); // Remove block comments
-					$css = preg_replace ( '#[^:\'"]//.*#', '', $css ); // Remove non url single line comments
-					$css = preg_replace ( '#^//.*#', '', $css ); // remove single line comments that start a line
-					$css = preg_replace ( '/\n/', "", $css ); // remove carriage returns
-					$css = preg_replace ( '/\s+/', " ", $css ); // remove any weird white space
-					if ($debug)
-						echo "<!-- Saving $ctargetfn (" . strlen ( $css ) . " bytes) -->\n";
-						file_put_contents ( $ctargetfn, $css );
+
+			// Write the uncompressed version for testing etc
+			file_put_contents ( $targetfn, $css );
+
+			// Pack and write the compressed version
+			$pcss = CssMin::minify ( $css );
+			if ($debug)
+				echo "<!-- Saving $ptargetfn (" . strlen ( $pcss ) . " bytes) -->\n";
+			file_put_contents ( $ptargetfn, $pcss );
+
+			$css = preg_replace ( '!/\*.*?\*/!s', '', $css ); // Remove block comments
+			$css = preg_replace ( '#[^:\'"]//.*#', '', $css ); // Remove non url single line comments
+			$css = preg_replace ( '#^//.*#', '', $css ); // remove single line comments that start a line
+			$css = preg_replace ( '/\n/', "", $css ); // remove carriage returns
+			$css = preg_replace ( '/\s+/', " ", $css ); // remove any weird white space
+			if ($debug)
+				echo "<!-- Saving $ctargetfn (" . strlen ( $css ) . " bytes) -->\n";
+			file_put_contents ( $ctargetfn, $css );
 		}
 	}
 }
@@ -896,7 +896,7 @@ function getBackTrace() {
 		$ret [] = $str;
 		$k ++;
 	}
-	
+
 	return $ret;
 }
 
