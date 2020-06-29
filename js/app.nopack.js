9b3104dabf4e70d69f94b624f429c223db57014d
Nigel Johnson <nigel@nigeljohnson.net>
2020-06-29 21:06:49 +0100

    Made getEnv call happen-every 5-seconds-ready

--- js/app.nopack.js
+++ js/app.nopack.js
@@ -415,16 +415,38 @@ app.controller('FooterCtrl', [ "$scope", function($scope) {
 	// This is only used to update the copyright year to "this year". Massive overkill.
 	$scope.nowDate = Date.now();
 } ]);
-app.controller('HomeCtrl', [ "$scope", "apiSvc", function($scope, apiSvc) {
-	$scope.loading = true;
-	$scope.title="Welcome Home";
-	logger("Started HomeCtrl");
-	apiSvc.call("getEnv", {}, function(data) {
-		logger("HomeCtrl::handleGetEnv()");
-		console.log(data);
-		if(data.message.length) {
-			toast(data.message);
-		}
-		$scope.loading = false;
-	}, true); // do post so response is not cached
-} ]);
+app.controller('HomeCtrl', [ "$scope", "$interval", "apiSvc",
+		function($scope, $interval, apiSvc) {
+			$scope.loading = true;
+			$scope.title = "Welcome Home";
+			logger("Started HomeCtrl");
+
+			// Turn this into a function and call it in the interval and before
+			// so it starts immediately.
+			// https://stackoverflow.com/a/21989838
+			var getEnv = function() {
+				apiSvc.call("getEnv", {}, function(data) {
+					logger("HomeCtrl::handleGetEnv()");
+					console.log(data);
+					if (data.message.length) {
+						toast(data.message);
+					}
+					$scope.loading = false;
+				}, true); // do post so response is not cached
+			};
+			getEnv();
+			// $scope.env_api_call = $interval(getEnv, 5000);
+
+			// OR much tidier, but won't fire until the first inteval
+			// $interval(function() {
+			// apiSvc.call("getEnv", {}, function(data) {
+			// logger("HomeCtrl::handleGetEnv()");
+			// console.log(data);
+			// if (data.message.length) {
+			// toast(data.message);
+			// }
+			// $scope.loading = false;
+			// }, true); // do post so response is not cached
+			// }, 5000) // 5000 ms execution
+
+		} ]);
