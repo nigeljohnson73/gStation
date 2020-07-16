143aa0e61a98047356210e038364e2e858373534
Nigel Johnson <nigel@nigeljohnson.net>
2020-07-16 02:02:14 +0100

    Worked out how to detect new display... hopefully

--- sh/oled_monitor.sh
+++ sh/oled_monitor.sh
@@ -5,20 +5,19 @@
 
 # TODO: Work ou twhy this ocilates and fix it
 # 0x3d is the 1.5" display, 0x3c is the 0.96" one
-#avail="$( i2cget -y 1 0x3d 2>&1 )"
+avail="$( i2cget -y 1 0x3d 2>&1 )"
 
-#if [ "$avail" = "0x41" ]; then
-#if [ "$avail" = "0x49" ]; then
+if [ "$avail" != "Error: Read failed" ]; then
 	echo "Using new display"
 	until python3 /webroot/gStation/sh/oled_1p5.py --height 128 --display ssd1327 --i2c-address 0x3d > /dev/null 2>&1; do
     		#echo "OLED driver crashed with exit code $?.  Respawning.." >&2
     		sleep 1
 	done
-#else
-#	echo "Using old display"
-#	until python3 /webroot/gStation/sh/oled_0p96.py > /dev/null 2>&1; do
-#    		#echo "OLED driver crashed with exit code $?.  Respawning.." >&2
-#    		sleep 1
-#	done
-#fi
+else
+	echo "Using old display"
+	until python3 /webroot/gStation/sh/oled_0p96.py > /dev/null 2>&1; do
+    		#echo "OLED driver crashed with exit code $?.  Respawning.." >&2
+    		sleep 1
+	done
+fi
 
